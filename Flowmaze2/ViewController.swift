61c10417730e6d30cadc95748bc04ade42a4550f
Knut Erik Borgen <keb@hospitality.no>
2017-12-26 22:31:57 +0100

    Initial Commit

---
+++ Flowmaze2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Flowmaze2
+//
+//  Created by Knut Erik Borgen on 26.12.2017.
+//  Copyright © 2017 Knut Erik Borgen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
