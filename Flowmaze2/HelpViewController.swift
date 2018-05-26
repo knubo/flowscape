180c8134796bfd9f6fbd7d2e38184347bea7a4c3
Knut Erik Borgen <keb@hospitality.no>
2018-05-26 19:45:37 +0200

    Preparing a help menu

---
+++ Flowmaze2/HelpViewController.swift
@@ -0,0 +1,34 @@
+import Foundation
+import UIKit
+import WebKit
+
+class HelpViewController: UIViewController, WKUIDelegate {
+    
+    @IBOutlet weak var subView: UIView!
+
+    var webView:WKWebView!
+    
+    override func loadView() {
+        super.loadView()
+        webView = WKWebView(frame: subView.bounds, configuration: WKWebViewConfiguration())
+        webView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        webView.uiDelegate = self
+        subView.addSubview(webView)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.automaticallyAdjustsScrollViewInsets = false
+        
+        
+        let url = Bundle.main.url(forResource: "index", withExtension: "html")!
+        webView.loadFileURL(url, allowingReadAccessTo: url)
+        let request = URLRequest(url: url)
+        webView.load(request)
+    }
+    
+    
+}
+
+
