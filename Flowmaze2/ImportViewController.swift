bbdab364028ddcb05c5b961f76baa9c0fdf2a93c
Knut Erik Borgen <keb@hospitality.no>
2018-05-27 15:52:50 +0200

    View for importing score

--- Flowmaze2/ImportViewController.swift
+++ Flowmaze2/ImportViewController.swift
@@ -10,19 +10,26 @@ import Foundation
 import AVFoundation
 import UIKit
 
-class ImportViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
+class ImportViewController: UIViewController {
 
-    var captureSession:AVCaptureSession?
+    @IBOutlet var messageLabel:UILabel!
+    @IBOutlet var backButton:UIButton!
+
+    var captureSession = AVCaptureSession()
     var videoPreviewLayer:AVCaptureVideoPreviewLayer?
     var qrCodeFrameView:UIView?
+    
+    private let supportedCodeTypes = [AVMetadataObject.ObjectType.qr]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+        // Get the back-facing camera for capturing videos
         let deviceDiscoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInDualCamera], mediaType: AVMediaType.video, position: .back)
         
         guard let captureDevice = deviceDiscoverySession.devices.first else {
-            print("Failed to get the camera device")
+            messageLabel.text = "Failed to get the camera device"
             return
         }
         
@@ -31,7 +38,16 @@ class ImportViewController: UIViewController, AVCaptureMetadataOutputObjectsDele
             let input = try AVCaptureDeviceInput(device: captureDevice)
             
             // Set the input device on the capture session.
-            captureSession?.addInput(input)
+            captureSession.addInput(input)
+            
+            // Initialize a AVCaptureMetadataOutput object and set it as the output device to the capture session.
+            let captureMetadataOutput = AVCaptureMetadataOutput()
+            captureSession.addOutput(captureMetadataOutput)
+            
+            // Set delegate and use the default dispatch queue to execute the call back
+            captureMetadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+            captureMetadataOutput.metadataObjectTypes = supportedCodeTypes
+            //            captureMetadataOutput.metadataObjectTypes = [AVMetadataObject.ObjectType.qr]
             
         } catch {
             // If any error occurs, simply print it out and don't continue any more.
@@ -39,26 +55,60 @@ class ImportViewController: UIViewController, AVCaptureMetadataOutputObjectsDele
             return
         }
         
-        // Initialize a AVCaptureMetadataOutput object and set it as the output device to the capture session.
-        let captureMetadataOutput = AVCaptureMetadataOutput()
-        captureSession?.addOutput(captureMetadataOutput)
-        
-
-        // Set delegate and use the default dispatch queue to execute the call back
-        captureMetadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
-        captureMetadataOutput.metadataObjectTypes = [AVMetadataObject.ObjectType.qr]
-        
-        
         // Initialize the video preview layer and add it as a sublayer to the viewPreview view's layer.
-        videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession!)
+        videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
         videoPreviewLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
         videoPreviewLayer?.frame = view.layer.bounds
         view.layer.addSublayer(videoPreviewLayer!)
         
         // Start video capture.
-        captureSession?.startRunning()
+        captureSession.startRunning()
+        
+        // Move the message label and top bar to the front
+        view.bringSubview(toFront: messageLabel)
+        view.bringSubview(toFront: backButton)
+        
+        // Initialize QR Code Frame to highlight the QR code
+        qrCodeFrameView = UIView()
+        
+        if let qrCodeFrameView = qrCodeFrameView {
+            qrCodeFrameView.layer.borderColor = UIColor.green.cgColor
+            qrCodeFrameView.layer.borderWidth = 2
+            view.addSubview(qrCodeFrameView)
+            view.bringSubview(toFront: qrCodeFrameView)
+        }
+
     }
     
+  
+}
+
+
+extension ImportViewController: AVCaptureMetadataOutputObjectsDelegate {
+    
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        // Check if the metadataObjects array is not nil and it contains at least one object.
+        if metadataObjects.count == 0 {
+            qrCodeFrameView?.frame = CGRect.zero
+            messageLabel.text = "No QR code is detected"
+            return
+        }
+        
+        // Get the metadata object.
+        let metadataObj = metadataObjects[0] as! AVMetadataMachineReadableCodeObject
+        
+        if supportedCodeTypes.contains(metadataObj.type) {
+            // If the found metadata is equal to the QR code metadata (or barcode) then update the status label's text and set the bounds
+            let barCodeObject = videoPreviewLayer?.transformedMetadataObject(for: metadataObj)
+            qrCodeFrameView?.frame = barCodeObject!.bounds
+            
+            if metadataObj.stringValue != nil {
+        //        launchApp(decodedURL: metadataObj.stringValue!)
+                messageLabel.text = "QR code found"
+            }
+        }
+    }
     
 }
+
 
