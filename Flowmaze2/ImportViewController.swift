e80e3e4e7952656e0a149899ec2915a14bc74121
Knut Erik Borgen <keb@hospitality.no>
2018-05-27 18:57:39 +0200

    import from qr code

--- Flowmaze2/ImportViewController.swift
+++ Flowmaze2/ImportViewController.swift
@@ -9,6 +9,7 @@
 import Foundation
 import AVFoundation
 import UIKit
+import MobileCoreServices
 
 class ImportViewController: UIViewController {
 
@@ -86,6 +87,75 @@ class ImportViewController: UIViewController {
 
 extension ImportViewController: AVCaptureMetadataOutputObjectsDelegate {
     
+    func parseJson(json:[String:Any]) {
+        
+        let pixel_width = json["pw"]
+        let pixel_height = json["ph"]
+        let board_size_x = json["bx"] is String ? (json["bx"] as! String) : String((json["bx"] as! Int))
+        let board_size_y = json["by"] is String ? (json["by"] as! String) : String((json["by"] as! Int))
+        let endTick = Int(json["tk"] as! String)!
+        
+        let level:String = json["ll"] as! String
+        let whenDate = json["wn"] as! String
+        let actions = json["ms"] as! String
+        // let checksum = json["cs"]
+        let name = json["sn"] as! String
+        
+        
+        let defaults = UserDefaults(suiteName: "group.flowmaze.knubo.no")!
+        
+        let myBoardSizeX = defaults.string(forKey:"board_size_x")
+        let myBoardSizeY = defaults.string(forKey:"board_size_y")
+        
+        
+        if(myBoardSizeX == nil) {
+            messageLabel
+                .text = "Play one round before importing score"
+            return
+        }
+        
+        if(board_size_x != myBoardSizeX || board_size_y != myBoardSizeY) {
+            messageLabel.text = "Game played on other device type than yours"
+            return
+        }
+        
+        
+        //VERIFY CHECKSUM ??
+        
+        var levels = defaults.stringArray(forKey:"levels") ?? [String]()
+        
+        if(!levels.contains(level)) {
+            levels.append(level)
+            defaults.set(levels, forKey:"levels")
+        }
+        
+        var who = defaults.stringArray(forKey:level) ?? [String]()
+        if(!who.contains(name)) {
+            who.append(name)
+            defaults.set(who, forKey:level)
+        }
+        
+        let tick = defaults.integer(forKey: level + "_tick_"+name)
+        
+        if(tick != 0 && tick <= endTick) {
+            messageLabel.text = "Higher score already registered"
+            return
+        }
+        
+        defaults.set(endTick, forKey: level + "_tick_"+name)
+        defaults.set(whenDate, forKey:level+"_when_"+name)
+        defaults.set(actions, forKey:level+"moves_"+name)
+        
+        
+        defaults.set(pixel_width, forKey: level+"_pixel_width_"+name)
+        defaults.set(pixel_height, forKey: level+"_pixel_height_"+name)
+        defaults.set(board_size_x, forKey: level+"_board_size_x_"+name)
+        defaults.set(board_size_y, forKey: level+"_board_size_y_"+name)
+        
+        messageLabel.text = "Score imported"
+        
+        
+    }
     func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
         // Check if the metadataObjects array is not nil and it contains at least one object.
         if metadataObjects.count == 0 {
@@ -104,10 +174,28 @@ extension ImportViewController: AVCaptureMetadataOutputObjectsDelegate {
             
             if metadataObj.stringValue != nil {
         //        launchApp(decodedURL: metadataObj.stringValue!)
-                messageLabel.text = "QR code found"
+                messageLabel.text = "QR code found"22
+            }
+            
+            if let data = metadataObj.stringValue!.data(using: String.Encoding.utf8) {
+                do {
+                    let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String:Any]
+                    
+                    if(json == nil) {
+                        messageLabel.text = "Failed to read data"
+                    } else {
+                        parseJson(json:json!);
+                    }
+                } catch {
+                    messageLabel.text = "JSON Parse error:" + metadataObj.stringValue!
+                    
+                }
+
             }
         }
+        
     }
+    
     
 }
 
